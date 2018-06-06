3599b2a99165ae11d730c8f1b69b7bd9f42ada96
Son <trinhson1998@gmail.com>
2018-06-06 07:33:06 +0700

    Initial Commit

---
+++ Slide Menu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slide Menu
+//
+//  Created by Son on 2018/06/06.
+//  Copyright © 2018 Son. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Slide MenuTests/Slide_MenuTests.swift
@@ -0,0 +1,36 @@
+//
+//  Slide_MenuTests.swift
+//  Slide MenuTests
+//
+//  Created by Son on 2018/06/06.
+//  Copyright © 2018 Son. All rights reserved.
+//
+
+import XCTest
+@testable import Slide_Menu
+
+class Slide_MenuTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
